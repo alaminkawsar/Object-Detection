6401644e4873b742ea117dcc123667e0273e160e
alaminkawsar <alaminkawsar8643@gmail.com>
2022-03-08 20:20:20 +0600

    yolo opencv model created

---
+++ yolo_opencv.py
@@ -0,0 +1,17 @@
+# import required packages
+import cv2
+import argparse
+import numpy as np
+
+# handle command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True,
+                help = 'path to input image')
+ap.add_argument('-c', '--config', required=True,
+                help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required=True,
+                help = 'path to yolo pre-trained weights')
+ap.add_argument('-cl', '--classes', required=True,
+                help = 'path to text file containing class names')
+args = ap.parse_args()
+
